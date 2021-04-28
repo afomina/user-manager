dbe7fe2024b78523ee3ed40a038e48066e354e24
Aleksandra Andrianova <aleksa.fom@gmail.com>
2021-04-28 16:25:07 -0400

    test fix

--- src/test/java/andrianova/usermanager/api/UserControllerTest.java
+++ src/test/java/andrianova/usermanager/api/UserControllerTest.java
@@ -72,7 +72,7 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.data.users[0].lastName", is("lastName")))
                 .andExpect(jsonPath("$.data.users[0].firstName", is("firstName")))
                 .andExpect(jsonPath("$.data.users[0].email", is("email@test.com")))
-                .andExpect(jsonPath("$.data.users[0].role", is("USER")));
+                .andExpect(jsonPath("$.data.users[0].role", is("user")));
     }
 
     @Test
@@ -96,7 +96,7 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.data.user.email", is("email@test.com")))
                 .andExpect(jsonPath("$.data.user.password.hash",
                         is("8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92")))
-                .andExpect(jsonPath("$.data.user.role", is("USER")))
+                .andExpect(jsonPath("$.data.user.role", is("user")))
                 .andExpect(jsonPath("$.data.user.avatar", is(Base64.getEncoder().encodeToString(getAvatar()))));
     }
 
